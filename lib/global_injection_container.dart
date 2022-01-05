bef81565c52af88e2df4bb23f8ab2a21a941ef72
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-01-05 15:14:04 -0300

    feat: configurando a injeção de dependencia

--- lib/global_injection_container.dart
+++ lib/global_injection_container.dart
@@ -1,7 +1,11 @@
+import 'package:quiz_car/app/home/home_injection_continer.dart';
 import 'package:quiz_car/core/i_injection_conetiner.dart';
 
 abstract class GlobalInjectionContainer {
-  static List<IInjectionContainer> get injectionsContainer => [];
+  static List<IInjectionContainer> get injectionsContainer => [
+        HomeInjectionContainer(),
+      ];
+
   static void setInjection() {
     for (IInjectionContainer item in injectionsContainer) {
       item();
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_car/app/my_app.dart';
+import 'package:quiz_car/global_injection_container.dart';
 
 void main() {
+  GlobalInjectionContainer.setInjection();
   runApp(const MyApp());
 }
