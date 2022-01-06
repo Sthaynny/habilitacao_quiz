e14ec3e375f356ab1ae4029e9133324c1e44a919
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-01-06 14:24:13 -0300

    feat: criando botão para acessar o quiz

--- lib/app/home/presentation/home_screen.dart
+++ lib/app/home/presentation/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adaptable_screen/adaptable_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quiz_car/app/home/presentation/components/car_quiz_widget.dart';
@@ -26,8 +27,41 @@ class _HomeScreen extends State<HomeScreen> {
         ),
         child: SingleChildScrollView(
           child: Column(
-            children: const [
-              CarQuizWidget(),
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.h),
+                child: const CarQuizWidget(),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                    AppColors.azul.withAlpha(180),
+                  ),
+                  elevation: MaterialStateProperty.all(3),
+                  fixedSize: MaterialStateProperty.all(
+                    Size(150.w, 200.h),
+                  ),
+                  shape: MaterialStateProperty.all(
+                    ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.r),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      AppImages.legislacao,
+                      width: 50.w,
+                    ),
+                    Text(
+                      "Legislação",
+                      style: AppTextStyles.notoSansBold(fontSize: 14.ssp),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
--- lib/core/styles/app_colors.dart
+++ lib/core/styles/app_colors.dart
@@ -8,5 +8,5 @@ abstract class AppColors {
   static const Color cinzaClaro = Color(0xFFA6A1B2);
   static const Color green = Color(0xFF0CF877);
   static const Color azulClaro = Color(0xFF00EDF0);
-  static const Color azul = Color(0xFF00EDF0);
+  static const Color azul = Color(0xFF0682DD);
 }
