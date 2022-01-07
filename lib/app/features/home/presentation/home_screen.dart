789e5a487e624b4246f374e57474e024c6b837e4
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-01-07 00:28:26 -0300

    refactor: ajustando titulo errado

--- lib/app/features/home/presentation/home_screen.dart
+++ lib/app/features/home/presentation/home_screen.dart
@@ -48,7 +48,7 @@ class _HomeScreen extends State<HomeScreen> with EmBreve {
                     bottomSheetEmBreve(context);
                   },
                   iconAsset: AppImages.direcaoDefensiva,
-                  titulo: "Direção defensiva",
+                  titulo: "Direção Defensiva",
                 ),
                 QuizButtonWidget(
                   onPressend: () {
