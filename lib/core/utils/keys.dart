de03d30208f6461c2748ec2b62e0b94d84468373
sthaynny <igorsthaynny@gmail.com>
2022-01-15 09:22:56 -0300

    feat: adicionando keys

--- lib/core/utils/keys.dart
+++ lib/core/utils/keys.dart
@@ -1,6 +1,8 @@
 abstract class Keys {
   static const direcaoDefensiva = 'direcao_defensiva';
   static const legislacao = 'legislacao';
+  static const meioAmbiente = 'meio_ambiente';
+  static const primeirosSocorros = 'legislaprimeiros_socorroscao';
 
   static const index = 'index';
 }
