603410b397a72a821a9ba6aedb9ceb3d634fbe27
sthaynny <igorsthaynny@gmail.com>
2022-01-15 09:26:15 -0300

    feat>

--- lib/app/features/home/home_injection_continer.dart
+++ lib/app/features/home/home_injection_continer.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
 import 'package:quiz_car/app/features/home/domain/usecases/direcao_defensiva_quiz_usercase.dart';
 import 'package:quiz_car/app/features/home/domain/usecases/legislacao_quiz_usercase.dart';
+import 'package:quiz_car/app/features/home/domain/usecases/meio_ambiente_quiz_usercase.dart';
+import 'package:quiz_car/app/features/home/domain/usecases/primeiros_socorros_quiz_usercase.dart';
 import 'package:quiz_car/app/features/home/presentation/controller/home_controller.dart';
 import 'package:quiz_car/core/i_injection_conetiner.dart';
 
@@ -11,10 +13,22 @@ class HomeInjectionContainer implements IInjectionContainer {
       () => DirecaoDefesivaQuizUsercase(Get.find()),
       fenix: true,
     );
+
     Get.lazyPut(
       () => LegislacaoQuizUsercase(Get.find()),
       fenix: true,
     );
+
+    Get.lazyPut(
+      () => MeioAmbienteQuizUsercase(Get.find()),
+      fenix: true,
+    );
+
+    Get.lazyPut(
+      () => PrimeirosSocorrosQuizUsercase(Get.find()),
+      fenix: true,
+    );
+
     Get.lazyPut<HomeController>(
       () => HomeController(
         direcaoDefesivaQuizUsercase: Get.find(),
