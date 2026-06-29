f2d4f77936093841b6f30d5d928905d0312a27b8
sthaynny <igorsthaynny@gmail.com>
2026-06-29 18:57:05 -0300

    chore: update Android dependencies and modify MainActivity for system window compatibility
    - Replaced 'androidx.activity:activity-ktx:1.10.1' with 'androidx.core:core-ktx:1.15.0' in build.gradle for improved functionality.
    - Changed edge-to-edge display implementation in MainActivity to use WindowCompat for better system window management.

--- android/app/src/main/kotlin/br/com/sthaynny/habilitacao_quiz/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/sthaynny/habilitacao_quiz/MainActivity.kt
@@ -1,12 +1,12 @@
 package br.com.sthaynny.habilitacao_quiz
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
     }
 }
